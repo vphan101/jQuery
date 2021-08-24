ac86bfea2f687b72eaf0730f9b2cae748a658722
vphan101 <58672453+vphan101@users.noreply.github.com>
2021-08-24 15:31:05 -0700

    Add files via upload

---
+++ DOM-app.js
@@ -0,0 +1,11 @@
+var d=document
+
+
+document.addEventListener("DOMContentLoaded", function(
+    event)
+{
+    console.log(d.body);
+    document.getElementById("my_btn").innerHTML="New Task"
+
+});
+    
---
+++ course.js
@@ -0,0 +1,25 @@
+console.log('Hello You');
+
+var name= "Mark";
+var obj= {
+    name: 'Mark',
+    site: 'Mark.com',
+    id: 1233,
+    type: true,
+
+};
+console.log(obj);
+
+
+var a1= [ 'Mark1', 'Mark2', 1, true];
+console.log(a1);
+
+
+var a2=[
+    {name:'John',id:1},
+    {name:'Make',id:2},
+    {name:'Duke',id:3},
+
+];
+console.log(a2);
+
